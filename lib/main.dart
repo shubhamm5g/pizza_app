3ff864445895c947a39e8e67b00f0000498707cb
shubham <shubhammaurya5g@gmail.com>
2024-02-07 01:17:25 +0530

    user package

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,4 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false ,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
---
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/src/models/user.dart';
+import 'package:user_repository/src/user_repo.dart';
+
+class FirebaseUserRepo implements UserRepository {
+  final FirebaseAuth _firebaseAuth;
+  final userCollection = FirebaseFirestore.instance.collection("users");
+
+  FirebaseUserRepo({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  @override
+  Future<void> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> setUserData(MyUser user) {
+    // TODO: implement setUserData
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signIn(String email, String password) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<MyUser?> signUp(MyUser myUser, String password) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement user
+  Stream<MyUser?> get user => throw UnimplementedError();
+}
